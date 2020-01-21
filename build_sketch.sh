fd5da8290baba28363ec17563f76d30de4f46182
David Chaves <dchavesd@gmail.com>
2020-01-21 13:55:24 +0100

    Added sketchbook path to build script

--- build_sketch.sh
+++ build_sketch.sh
@@ -1,3 +1,4 @@
 FQBN=esp32:esp32:esp32doit-devkit-v1
-SKETCH=$1
-docker run -v $PWD/sketchbook:/sketchbook -it arduino-cli compile --fqbn $FQBN $SKETCH
+SKETCKBOOK=$1
+SKETCH=$2
+docker run -v $PWD/$SKETCHBOOK:/sketchbook -it arduino-cli compile --fqbn $FQBN $SKETCH
