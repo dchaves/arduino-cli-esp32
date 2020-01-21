f20278701e1b5f8250d6957d73a73667cf1a2495
David Chaves <dchavesd@gmail.com>
2020-01-21 13:58:05 +0100

    Added sketchbook path to build script

--- upload_sketch.sh
+++ upload_sketch.sh
@@ -1,4 +1,5 @@
 FQBN=esp32:esp32:esp32doit-devkit-v1
-SKETCH=$1
+SKETCKBOOK=$1
+SKETCH=$2
 DEV=/dev/ttyUSB0
-docker run -v $PWD/sketchbook:/sketchbook -it arduino-cli upload -p $DEV --fqbn $FQBN $SKETCH
+docker run -v $PWD/$SKETCHBOOK:/sketchbook -it arduino-cli upload -p $DEV --fqbn $FQBN $SKETCH
