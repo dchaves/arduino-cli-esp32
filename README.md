48352416061b899e77e3f0ca3032aa546e14e9bb
David Chaves <dchavesd@gmail.com>
2020-01-24 17:24:39 +0100

    Controller stub

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Docker image and utilities to compile and upload esp32 binaries using [arduino-c
 # Usage
 1. Install dependencies (Docker version 19.03.5)
 2. Run ./build_docker.sh
-3. Create an sketch. If you want to create a stub sketch, you can run `docker run -v $PWD/[SKETCHBOOK_PATH] -it arduino-cli sketch new [SKETCH_NAME]`
+3. Create an sketch. If you want to create a stub sketch, you can run `docker run -v $PWD/[SKETCHBOOK_PATH]:/sketchbook -it arduino-cli sketch new [SKETCH_NAME]`
 3. Run ./build_sketch.sh [SKETCHBOOK_PATH_RELATIVE_TO_CURRENT_DIR] [SKETCH_NAME]
 4. Run ./upload_sketch.sh [SKETCHBOOK_PATH_RELATIVE_TO_CURRENT_DIR] [SKETCH_NAME]
--- sketchbook/esp32_controller/esp32_controller.ino
+++ sketchbook/esp32_controller/esp32_controller.ino
@@ -14,52 +14,43 @@
 //915E6 for North America
 #define BAND 866E6
 
-// CPU #0
-// Perform cycle:
-// 1. Receive each motor duty via LoRa
-// 2. Set PWM of each motor
-void control_task(void* param) {
-    vTaskDelete(NULL);
-}
-
-// CPU #1
-// Perform cycle:
-// 1. Read telemetry from ESC
-// 2. Send telemetry via LoRa
-void telemetry_task(void* param) {
-    while(1){
-        // Read telemetry from ESC
-        // Do Stuff
-
-        // Send LoRa packet to receiver
-        LoRa.beginPacket();
-        LoRa.write(buffer, length); // MAX 255 bytes
-        LoRa.endPacket();
-    }
-    vTaskDelete(NULL);
-}
-
-// When a packet is received, configure both ESC PWMs.
+// When a packet is received, send it via USB
 void onReceive(int packetSize) {
     //Do stuff
     Serial.print("Received packet with RSSI ");
     Serial.println(LoRa.packetRssi());
 }
 
-// Init ESC comms
-// Init LoRa comms
-// Init telemetry and control tasks
+// CPU #0
+void telemetry_task(void* param) {
+    vTaskDelete(NULL);
+}
+
+// CPU #1
+// Perform cycle:
+// 1. Read commands from USB
+// 2. Send commands via LoRa
+void control_task(void* param) {
+    while(1){
+        // Read commands from USB
+        // Do Stuff
+        if (Serial.available() > 0) {
+            incomingByte = Serial.read();
+            
+            // Send LoRa packet to receiver
+            LoRa.beginPacket();
+            LoRa.write(buffer, length); // MAX 255 bytes
+            LoRa.endPacket();
+        }
+    }
+    vTaskDelete(NULL);
+}
+
 void setup() {
     //Initialize Serial comms
     Serial.begin(115200);
-    Serian.println("Starting Gooseka...");
+    Serian.println("Starting controller...");
 
-    // ESC read init
-    // Do stuff
-
-    // ESC PWM init
-    // Do stuff
-    
     //SPI LoRa pins
     SPI.begin(SCK, MISO, MOSI, SS);
     //setup LoRa transceiver module
@@ -79,6 +70,6 @@ void setup() {
 }
 
 // Do nothing
-void loop {
+void loop() {
     delay(2147483647L);
 }
