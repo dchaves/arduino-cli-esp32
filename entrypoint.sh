3bd00b2ee49b8b134bf80f75fd9a2a83f3bda653
David Chaves <dchaves@gradiant.org>
2020-02-21 16:37:14 +0100

    Added action parameters

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -l
+ACTION=$1
+shift
+SKETCH=$1
+shift
+BOARD=$1
+shift
+
+ln -s /sketchbook/workspace /sketchbook/$SKETCH
+
+if [ "$ACTION" = "compile" ]; then
+	LIBRARIES=$@
+	if [ ! -z "$LIBRARIES" ]; then /arduino/bin/arduino-cli lib install $LIBRARIES; fi
+	/arduino/bin/arduino-cli $ACTION --fqbn $BOARD $SKETCH
+elif [ "$ACTION" = "upload" ]; then
+	DEV=$1
+	shift
+	LIBRARIES=$@
+	if [ ! -z "$LIBRARIES" ]; then /arduino/bin/arduino-cli lib install $LIBRARIES; fi
+	/arduino/bin/arduino-cli $ACTION -p $DEV --fqbn $BOARD $SKETCH
+else
+	echo "Unknown action"
+	exit 1
+fi
